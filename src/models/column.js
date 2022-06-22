2539e8bd4b628246144f30f1b3e92073670a2976
rajpranz <rajeshwarrprasad@yahoo.com>
2022-06-22 13:37:12 +1200

    Setup column object

--- src/models/column.js
+++ src/models/column.js
@@ -1,6 +1,20 @@
-export default function Column(name){
+import {idGenerator} from '../idGenerator';
+
+export default function Column(name, idGenerator){
     let _name = name;
-    let tasks = [];
+    let _tasks = [];
     let _type = "column";
-    let _id = new Date().getDate();
+    let _id = idGenerator();
+
+    return{
+        set name(columnName){
+            let val = columnName.trim();
+            if(val.length == 0)
+                throw "Column name cant be blank";
+            _name = val;
+        },
+        get name(){
+            return
+        }
+    }
 }
