d68ccb4c7a553bc0c8c572161eadcc9933f4acd6
rajpranz <rajeshwarrprasad@yahoo.com>
2022-06-29 17:26:54 +1200

    Update board title

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './style.css';
 import Task from  './models/task';
+import Board from './models/board';
 import TaskStatus from "./taskStatus";
 import {todoList, doingList, doneList} from "./dom/domHelpers";
-import {appendListItem, appendListItem2, clearList, generateColumnHeaders, generateBoardButtons} from "./dom/domFunctions";
+import {appendListItem, appendListItem2, clearList, generateColumnHeaders, generateBoardButtons, generateBoardItems} from "./dom/domFunctions";
 import {idGenerator} from './idGenerator';
 import { generateDummyData } from './dummydata';
 import { board as platformLaunch} from './dummydata';
@@ -11,22 +12,28 @@ let data = [];
 let currentBoard = null;
 let content = document.querySelector('.content');
 let boards = document.querySelector('.boards');
+let boardName = document.querySelector('.headerText');
 
 //Use dummy data for testing
 data.push(generateDummyData());
 currentBoard = data[0];
 
+let newBoard = new Board ("Test Board", idGenerator);
+data.push(newBoard);
+
+//console.log(currentBoard.name);
+
+setBoardName(boardName, currentBoard.name);
 
 generateColumnHeaders(content, currentBoard.columns);
 
 generateBoardButtons(boards, data);
 
-currentBoard.columns.forEach(column => {
-    let targetName=column.name.toLowerCase();
-    column.tasks.forEach(task => {
-        appendListItem2(targetName, task);
-        
-    });
-    
-});
+generateBoardItems(currentBoard);
+
+
+function setBoardName(boardName, name){
+    boardName.innerText = name;
+}
+
 console.log('It worked');
